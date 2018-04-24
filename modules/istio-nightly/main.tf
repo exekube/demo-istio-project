39d440fcaa2d224ac0b0bb05bc3a5382310430fe
Ilya Sotkov <ilya@sotkov.com>
2018-04-24 13:30:22 +0300

    [Broken] Update istio-nightly module

--- modules/istio-nightly/main.tf
+++ modules/istio-nightly/main.tf
@@ -10,10 +10,19 @@ module "istio_nightly" {
   tiller_namespace = "kube-system"
   client_auth      = "${var.secrets_dir}/kube-system/helm-tls"
 
-  release_name  = "istio"
-  chart_repo    = "exekube"
-  chart_name    = "istio"
-  chart_version = "0.8.0"
+  release_name      = "istio"
+  release_namespace = "istio-system"
+  chart_repo        = "exekube"
+  chart_name        = "istio"
+  chart_version     = "0.8.0"
 
   load_balancer_ip = "${var.load_balancer_ip}"
+}
+
+resource "null_resource" "enable_injection" {
+  depends_on = ["module.istio_nightly"]
+
+  provisioner "local-exec" {
+    command = "kubectl label namespace default istio-injection=enabled"
+  }
 }
