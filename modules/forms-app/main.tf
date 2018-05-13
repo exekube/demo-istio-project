34ac2d8b66f32c4c689458ac83bd83f57c7fc015
Ilya Sotkov <ilya@sotkov.com>
2018-05-13 11:46:06 +0300

    Add railsgoat module

--- modules/forms-app/main.tf
+++ modules/forms-app/main.tf
@@ -2,15 +2,20 @@ terraform {
   backend "gcs" {}
 }
 
-variable "domain_name" {}
+# variable "domain_name" {}
+# variable "secrets_dir" {}
+# 
+#
+# module "forms_app" {
+#   source           = "/exekube-modules/helm-release"
+#   tiller_namespace = "kube-system"
+#   client_auth      = "${var.secrets_dir}/kube-system/helm-tls"
+#
+#   release_name      = "forms-app"
+#   release_namespace = "default"
+#   chart_repo        = "exekube"
+#   chart_name        = "nginx-react"
+#   chart_version     = "0.2.0"
+#   domain_name       = "${var.domain_name}"
+# }
 
-module "forms_app" {
-  source = "/exekube-modules/helm-template-release"
-
-  release_name      = "forms-app"
-  release_namespace = "default"
-  chart_repo        = "exekube"
-  chart_name        = "nginx-react"
-  chart_version     = "0.2.0"
-  domain_name       = "${var.domain_name}"
-}
--- modules/railsgoat/main.tf
+++ modules/railsgoat/main.tf
@@ -3,13 +3,16 @@ terraform {
 }
 
 variable "secrets_dir" {}
+variable "domain_name" {}
 
-module "bookinfo" {
+module "railsgoat" {
   source           = "/exekube-modules/helm-release"
   tiller_namespace = "kube-system"
   client_auth      = "${var.secrets_dir}/kube-system/helm-tls"
 
-  release_name      = "bookinfo"
+  release_name      = "railsgoat"
   release_namespace = "default"
-  chart_name        = "bookinfo-v1alpha2/"
+  domain_name       = "${var.domain_name}"
+
+  chart_name = "railsgoat/"
 }
