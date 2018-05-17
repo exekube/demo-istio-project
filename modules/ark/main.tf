dfc8a6eed77642c643608d008f52d3cae6b0192e
Ilya Sotkov <ilya@sotkov.com>
2018-05-17 07:06:41 +0300

    Add ark with gcs buckets, fix problems with injection in postgres

---
+++ modules/ark/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "gcs" {}
+}
+
+variable "project_id" {}
+variable "secrets_dir" {}
+
+module "ark" {
+  source = "/exekube-modules/helm-template-release"
+
+  release_name      = "ark"
+  release_namespace = "kube-system"
+
+  chart_name = "ark/"
+  project_id = "${var.project_id}"
+}
+
+resource "null_resource" "ark_credentials" {
+  provisioner "local-exec" {
+    command = <<EOF
+kubectl create secret generic cloud-credentials \
+--namespace heptio-ark \
+--from-file cloud=${var.secrets_dir}/kube-system/credentials-ark
+EOF
+  }
+}
